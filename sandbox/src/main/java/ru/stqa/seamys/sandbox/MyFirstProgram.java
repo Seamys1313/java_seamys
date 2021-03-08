f87b0424501e002e9ec26d109040f5bece15f6b0
seamys1313 <faiblefemme@gmail.com>
2021-03-08 18:39:48 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/seamys/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/seamys/sandbox/MyFirstProgram.java
@@ -1,8 +1,33 @@
 package ru.stqa.seamys.sandbox;
 
 public class MyFirstProgram {
-  
-  public static  void main(String[] args){
-	System.out.println("Hello, world");
-  }
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Victory");
+
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+
+        double a = 2;
+        double b = 3;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+
+    }
+
+
+    public static double area(double l) {
+        return l * l;
+    }
+    public static double area(double a,double b) {
+        return a * b;
+    }
 }
+
+
