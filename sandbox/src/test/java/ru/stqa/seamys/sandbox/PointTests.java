b495a864a560f60134cb046e94f972bd2edfa731
seamys1313 <faiblefemme@gmail.com>
2021-03-14 18:11:43 +0300

    Добавлен тест для класса Point

--- sandbox/src/test/java/ru/stqa/seamys/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/seamys/sandbox/PointTests.java
@@ -9,8 +9,8 @@ public class PointTests {
     @Test
     public void testDistance() {
 
-        Point p1 = new Point(5, 6);
-        Point p2 = new Point(6, 7);
+        Point p1 = new Point(5.0,6.0 );
+        Point p2 = new Point(6.0, 7.0);
         Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
 
     }
